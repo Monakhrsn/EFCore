6aa7651e22cfb68abad3000590a11896280b54e6
Mona Khorasani <monakhrsn@gmail.com>
2025-01-22 11:51:07 +0100

    The business and presentation folder and more

---
+++ Presentation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Authentication.ExtendedProtection;
+using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+
+// register in our service
+
+serviceCollection.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(
+        @"Server=localhost;Database=EFCoreDB;User Id=sa;Password=VerySecret1234;TrustServerCertificate=True;"));
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
